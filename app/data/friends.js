9df4c4ba430002a6735f1e6ceb879698aa95a137
unknown <sundaresh.sankaran@gmail.com>
2018-09-11 17:03:10 -0400

    HW

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friendArray = [
+
+{
+    name:"Freddie Mercury",
+    photo:"https://metrouk2.files.wordpress.com/2016/08/88426684-e1472551231488.jpg?quality=80&strip=all",
+    scores:[5,4,3,2,3,2]
+},
+
+{
+    name:"Brian May",
+    photo:"https://ichef.bbci.co.uk/images/ic/960x540/p01bqmwg.jpg",
+    scores:[3,3,4,2,4,5]
+},
+
+{
+    name:"Brian Eno",
+    photo:"http://thestereolounge.files.wordpress.com/2012/05/brian-eno-roxy-music-photo-shoot-circa-1972.jpg",
+    scores:[5,5,5,4,5,4]
+},
+
+{
+    name:"Talking Heads",
+    photo:"https://media.gettyimages.com/photos/talking-heads-posed-in-amsterdam-netherlands-in-june-1977-lr-chris-picture-id94101309?s=612x612",
+    scores:[3,4,5,5,4,5]
+},
+
+{
+    name:"Head Detective Adrian Monk",
+    photo:"https://78.media.tumblr.com/8f978e7259be865012a89b09f1e7de45/tumblr_inline_nfxezzb7Du1qlr65v.jpg",
+    scores:[4,5,2,5,4,5]
+}
+
+];
+
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendArray;
---
+++ server.js
@@ -0,0 +1,76 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var friendArray = require("./app/data/friends.js");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+console.log("Expressed");
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = 8080;
+
+// // Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+// require("./data/routing/apiRoutes.js")(app);
+
+app.get("/", function(req, res) {
+    console.log("inside");
+    res.sendFile( path.join(__dirname,"/app/public/home.html"));
+  });
+
+app.get("/survey", function(req, res) {
+    console.log("inside");
+    res.sendFile( path.join(__dirname,"/app/public/survey.html"));
+  });
+
+app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body-parser middleware
+    scoreArray=[];
+    console.log(req.body);
+    
+    for (var a = 0; a < friendArray.length; a++){
+        finalscore=0;
+        for(var b = 0 ; b < friendArray[a].scores.length; b++){
+            finalscore=finalscore+Math.pow(req.body.scores[b]-friendArray[a].scores[b],2);
+        };
+        scoreArray.push(finalscore);
+        console.log(scoreArray);
+    };
+
+    selectedFriend = friendArray[scoreArray.indexOf(Math.min.apply(null, scoreArray))];
+    res.json(selectedFriend);
+    
+
+  });
+
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
